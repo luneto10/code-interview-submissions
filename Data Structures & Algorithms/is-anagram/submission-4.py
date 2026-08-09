0f3ac96dbc5313ccfb6e456b6621892a12509bf3
Luciano Carvalho <lucianocarvalhodev@gmail.com>
2026-08-09 12:59:31 -0700

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_letter_dict = Counter(s)
+        t_letter_dict = Counter(t)
+
+        # for k, v in s_letter_dict.items():
+        #     t_value = t_letter_dict.get(k)
+        #     if not t_value or t_value != v:
+        #         return False
+        return t_letter_dict == s_letter_dict
+            
